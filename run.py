ed0e16074745227b02952a638d74630e8625a672
Rodrigo Vazquez <r.vazquezasenjo@gmail.com>
2024-09-07 14:22:29 +0000

    first commit: API imported from Google Workspace'

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+#importing the main function from quickstart to obtain the upcoming events
+from quickstart import main
+
+#Initial function to run when you open the program. It prompts you with an event you'd like to add. 
+    #Potentially it should just prompt you with the option to run?
+def tool_start():
+    event_input = input ("Welcome! Please enter your event below: \n")
+    return event_input
+def add_event():
+    print ("something")
+
+
+tool_start()
