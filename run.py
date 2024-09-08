44d23371378ef3bf3fdde6be5a5849ab61e16e7b
Rodrigo Vazquez <r.vazquezasenjo@gmail.com>
2024-09-08 10:47:12 +0000

    data validation for time duration added

--- run.py
+++ run.py
@@ -5,6 +5,7 @@
 
 #importing the main function from quickstart to obtain the upcoming events
 from quickstart import main
+from datetime import datetime
 
 #Initial function to run when you open the program. It prompts you with an event you'd like to add. 
     #Potentially it should just prompt you with the option to run?
@@ -12,14 +13,46 @@ from quickstart import main
 
 def collect_event_title ():
     while True:
-        event_summary = input (('Welcome! Please enter your event below: \n'))
+        event_summary = input (('Please enter your event below: \n'))
         if event_summary == '':
-            print ('You need to add a title!\n')
-        elif len(event_summary) >= 10:
-            print (f'Too long! Maximum 10 characters, you typed {len(event_summary)} characters\n')
+            raise ValueError ('You need to add a title!\n')
+        elif len(event_summary) > 100:
+            raise ValueError (f'Too long! Maximum 100 characters, you typed {len(event_summary)} characters\n')
         else: break
-        
+    print("Nice! Let's move to the next step\n")
+    return event_summary    
+
+def collect_event_priority ():
+    while True:
+        print ("Event pririoties are ranked from 1 to 5, 1 being lowest priority and 5 the highest.\n")
+        event_priority = input (('Enter your event priority here: \n'))
+        if event_priority == '':
+            raise ValueError ('Whoops! You need to enter a priority.\n')
+        try:
+            if int(event_priority)>= 1 or int(event_priority)<= 5:
+                break
+        except ValueError as e:
+            print(f'Watchout! You entered {event_priority}, it should be between 1 and 5')
+    print("Nice! Let's move to the next step\n")
+    return event_priority 
+
+def collect_event_duration():
+    while True:
+        event_duration = input('How long will the event last: \n')
+        if event_duration == '':
+            raise ValueError("Oh no! Seems like you didn't enter a duration, try again!.\n")
+        try:
+            event_duration = datetime.strptime(event_duration, '%H:%M')
+            break
+        except ValueError as e:
+            print('The duration should be in hours and minutes (hours:minutes format), please try again!')
+    print("Nice! Let's move to the next step\n")
+    return event_duration
+
 def tool_start():
+    print ("Hi! Let's get the first item of the event\n")
     collect_event_title()
+    collect_event_priority()
+    collect_event_duration()
 
 tool_start()
