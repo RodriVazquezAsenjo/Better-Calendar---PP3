fb8f7dcda3755211430a2d6176afd78f21387810
Rodrigo Vazquez <r.vazquezasenjo@gmail.com>
2024-09-08 11:05:55 +0000

    data validation for deadline added

--- run.py
+++ run.py
@@ -38,7 +38,7 @@ def collect_event_priority ():
 
 def collect_event_duration():
     while True:
-        event_duration = input('How long will the event last: \n')
+        event_duration = input('Using a hours:minutes format, how long will the event last: \n')
         if event_duration == '':
             raise ValueError("Oh no! Seems like you didn't enter a duration, try again!.\n")
         try:
@@ -49,10 +49,24 @@ def collect_event_duration():
     print("Nice! Let's move to the next step\n")
     return event_duration
 
+def collect_event_deadline():
+    while True:
+        event_deadline = input('Using a day/month/year hours/min format, when is the deadline for this project? \n')
+        if event_deadline == '':
+            raise ValueError("Oh no! Seems like you didn't enter a deadline, try again!.\n")
+        try:
+            event_deadline = datetime.strptime(event_deadline, '%d/%m/%Y %H:%M')
+            break
+        except ValueError as e:
+            print('The deadline should be in date, hours and minutes (day/month/year hours:minutes format), please try again!')
+    print("Nice! Let's move to the next step\n")
+    return event_deadline
+
 def tool_start():
     print ("Hi! Let's get the first item of the event\n")
     collect_event_title()
     collect_event_priority()
     collect_event_duration()
+    collect_event_deadline()
 
 tool_start()
