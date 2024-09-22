c63d46e0c6709b172aa9fe53e34310697cc143ae
Rodrigo Vazquez <r.vazquezasenjo@gmail.com>
2024-09-22 03:31:53 +0000

    fix: port set to 8000 instead of 0

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ def authenticate_google_calendar():
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
             )
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=8000)
         with open("token.json", "w") as token:
             token.write(creds.to_json())
     return creds
